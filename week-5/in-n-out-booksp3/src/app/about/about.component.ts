13b938957fe1cf95c79fe903073dab793e54f7f3
Arlix Sorto <ar2694@outlook.com>
2021-01-30 14:23:11 -0600

    Assignment 5.4

---
+++ week-5/in-n-out-booksp3/src/app/about/about.component.ts
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: about.component.ts
+; Author: Professor Krasso
+; Date: 29 January 2021
+; Modified By: Arlix Sorto
+; Description: Exercise 5.2 - Navigation and Layout
+;===========================================
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
