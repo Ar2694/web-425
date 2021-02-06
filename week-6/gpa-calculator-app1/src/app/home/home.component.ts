386e95c7753b4b326d75cab8aac5cb1b53d8c44d
Arlix Sorto <ar2694@outlook.com>
2021-02-06 17:03:35 -0600

    Exercise 6.3

---
+++ week-6/gpa-calculator-app1/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: home.component.ts
+; Author: Professor Krasso
+; Date: 06 Feburary 2021
+; Modified By: Arlix Sorto
+; Description: Exercise 6.3 - Layouts
+;===========================================
+*/ 
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
