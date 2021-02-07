d7bef0e06b80ee7967ad863acdaed4c320776ccd
Arlix Sorto <ar2694@outlook.com>
2021-02-06 23:04:44 -0600

    Assignment 6.4

---
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gpa',
+  templateUrl: './gpa.component.html',
+  styleUrls: ['./gpa.component.scss']
+})
+export class GpaComponent implements OnInit {
+
+  @Input() gpaTotal: number;
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
